f04ecc54b0ae8ed9e54d8467e24387b788cf38be
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-12-26 23:28:09 +0600

    operator

---
+++ operators.dart
@@ -0,0 +1,24 @@
+main(List<String> args) {
+  // Addition Operator 
+  int a = 10;
+  int b = 20;
+  print(a+b);
+  // Substruction
+  print(b-a);
+  // Multiplication
+  print(a*b);
+  // Divition
+  print(b/a);
+  // Remainder
+  print(b%a);
+
+  // Equality and Relational Operator -> Needed in conditional expression it retuen eighter true or false
+  //  == , != , >= , <= , < , > 
+  print(a==b);
+  print(a!=b);
+  print(a>b);
+  print(a<b);
+  print(a>=b);
+  print(a<=b);
+
+}
--- veriable.dart
+++ veriable.dart
@@ -1,4 +1,12 @@
 main(List<String> args) {
+  /*
+  String -> var
+  Number -> int , double
+  Boolean - > true , false
+  List
+  Maps
+  */
+  
   var name = 'Mortuza ';
   int number = 10;
   // Print Name
@@ -7,4 +15,32 @@ main(List<String> args) {
   print(number);
   // Concatnat number and string
   print(name + number.toString());
+
+  num a = 10;
+  int b = 100;
+  double c = 100;
+  bool isTrue = true;
+  bool falseme = false;
+
+  /*
+  Constant and Final
+  Constant -- Remain same in compile time
+  Final -- Always be the same value // It can be set once
+  */
+  const pi = 3.1416;
+  final pii = 3.1416;
+
+  /*
+  Variable Concatanation
+  */
+
+  var firstName = "Mortuza";
+  var lastName = "Hossain";
+  int age = 21;
+
+  print("Details of user : $firstName\n First Name : $firstName \n Last Name : $lastName\n Age : $age");
+
+  // First Name prit as uppercase using method
+  print("${name.toUpperCase()}");
+
 }
